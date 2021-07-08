d19b17f36387c7a0647a23affa93d36bea7572db
Mark Needham <m.h.needham@gmail.com>
2021-07-08 15:42:06 +0100

    by org

--- app.py
+++ app.py
@@ -6,54 +6,40 @@ from pinotdb import connect
 st.set_page_config(layout="wide", page_title="GitHub Events")
 st.title("GitHub Events")
 
-
 conn = connect(host='localhost', port=8000, path='/query/sql', scheme='http')
 
+@st.cache
+def get_orgs():
 
-columns = ["numLinesAdded", "numFilesChanged", "numLinesDeleted", "numComments"]
-
-def run_query(column):
-    curs = conn.cursor()
-    curs.execute(f"""
-    select organization, repo, 
-        sum({column}) as {column}Sum
+    orgs_query = """
+    select organization, count(*) AS count
     from pullRequestMergedEvents 
-    GROUP BY organization, repo
-    ORDER BY {column}Sum DESC
-    limit 20
-    """)
+    group by organization
+    order by count DESC
+    limit 50
+    """
+
+    curs = conn.cursor()
+    curs.execute(orgs_query)
     df = pd.DataFrame(curs, columns=[item[0] for item in curs.description])
-    df.loc[:, 'name'] = df['repo'].str.cat(df['organization'], sep='/')
-    return df
+    return df['organization'].values
 
-metrics = columns
+org = st.selectbox('Choose an organization', get_orgs())
 
+metrics = ["numLinesAdded", "numFilesChanged", "numLinesDeleted", "numComments"]
 
-left, right = st.beta_columns(2)    
-with left:
-    for metric in [f for idx, f in enumerate(metrics) if idx % 2 == 0]:        
-        with st.spinner("Loading chart..."):
-            st.header(f"Top repositories by {metric}")
-            df = run_query(metric)
-            metric = f"{metric}Sum"            
-            chart = alt.Chart(df, padding={"left": 10, "top": 10, "right": 10, "bottom": 10}).mark_bar().encode(
-                y=alt.Y(metric, axis=alt.Axis(labels=True, ticks=False), title=None),
-                x=alt.X('name', sort=[metric], title="Repository", axis=alt.Axis(labelAngle=45)),
-                tooltip=[alt.Tooltip(metric, format=','), "name"]
-            ).properties(height=500)
+curs = conn.cursor()
+curs.execute(f"""
+select sum(numLinesAdded) AS linesAdded,
+       sum(numLinesDeleted) AS linesDeleted,
+       sum(numFilesChanged) AS filesChanged,
+       sum(numCommits) AS commits
+from pullRequestMergedEvents 
+WHERE organization = '{org}'
+order by count DESC
+limit 50
+""")
 
-            st.altair_chart(chart, use_container_width=True)
+df = pd.DataFrame(curs, columns=[item[0] for item in curs.description])
 
-with right:
-    for metric in [f for idx, f in enumerate(metrics) if idx % 2 != 0]:
-        with st.spinner("Loading chart..."):
-            st.header(f"Top repositories by {metric}")
-            df = run_query(metric)
-            metric = f"{metric}Sum"
-            chart = alt.Chart(df, padding={"left": 10, "top": 10, "right": 10, "bottom": 10}).mark_bar().encode(
-                y=alt.Y(metric, axis=alt.Axis(labels=True, ticks=False), title=None),
-                x=alt.X('name', sort=[metric], title="Repository", axis=alt.Axis(labelAngle=45)),
-                tooltip=[alt.Tooltip(metric, format=','), "name"]
-            ).properties(height=500)
-
-            st.altair_chart(chart, use_container_width=True)
+st.dataframe(df.assign(hack='').set_index('hack'))
