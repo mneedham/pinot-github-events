f2ea098828bb946a6e45f4b79766edaea8802e4c
Mark Needham <m.h.needham@gmail.com>
2021-07-14 14:57:18 +0100

    time based filtering

--- overview.py
+++ overview.py
@@ -4,36 +4,39 @@ import streamlit as st
 from pinotdb import connect
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import time
 
 def overview():
-    st.title("GitHub Events: An Overview")
+    st.title("Overview")
 
     conn = connect(host='localhost', port=8000, path='/query/sql', scheme='http')
 
-    def get_orgs_df(metric):
+    def get_orgs_df(metric, subtract_time):
         orgs_query = f"""
         select organization, 
             {metric}
         from pullRequestMergedEvents 
+        where createdTimeMillis > cast((now() - %(subtract_time)d*1000*60) as long)
         group by organization
         order by {metric} DESC
         limit 1000
         """
         curs = conn.cursor()
-        curs.execute(orgs_query, {"metric": metric})
+        curs.execute(orgs_query, {"subtract_time": subtract_time})
         return pd.DataFrame(curs, columns=[item[0] for item in curs.description])    
 
-    def get_repos_df(metric):
+    def get_repos_df(metric, subtract_time):
         orgs_query = f"""
         select organization, repo, 
             {metric}
         from pullRequestMergedEvents 
+        where createdTimeMillis > cast((now() - %(subtract_time)d*1000*60) as long)
         group by organization, repo
         order by {metric} DESC
         limit 1000
         """
         curs = conn.cursor()
-        curs.execute(orgs_query, {"metric": metric})
+        curs.execute(orgs_query, {"subtract_time": subtract_time})
         return pd.DataFrame(curs, columns=[item[0] for item in curs.description])
 
     metrics = [
@@ -45,6 +48,12 @@ def overview():
         "sum(numReviewComments)",
         "avg(numReviewers)"
     ]
+
+    time_range = {
+        "All events": round(time.time() * 1000),
+        "Last hour": 60,
+        "Last 12 hours": 12*60
+    }
 
 
     query = """
@@ -70,28 +79,27 @@ def overview():
     | {df['linesDeleted'].astype('int32').values[0]:,}
     """)
 
-    st.header("Most active organisations")
-    left, right = st.beta_columns([1,4]) 
-    with left:
-        metric = st.selectbox('Select a metric', metrics) 
+    st.markdown("---")
 
-    with right:    
-        with st.spinner('Loading word cloud...'):
-            wc = WordCloud(background_color="white", max_words=1000, width=800, height=500)        
-            df = get_orgs_df(metric)
-            wc.generate_from_frequencies({item[0]: item[1] for item in df.values})
-            st.image(wc.to_array())
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        metric = st.selectbox('Select a metric:', metrics)
+    with col2:
+        timestamp = st.selectbox('Select time range:', list(time_range.keys()))
+
+    st.subheader("Most active organisations")
+    with st.spinner('Loading word cloud...'):
+        wc = WordCloud(background_color="white", max_words=1000, width=800, height=500)        
+        df = get_orgs_df(metric, time_range[timestamp])
+        wc.generate_from_frequencies({item[0]: item[1] for item in df.values})
+        st.image(wc.to_array())
 
 
-    st.header("Most active repositories")
-    left, right = st.beta_columns([1,4])    
-    with left:
-        metric = st.selectbox('Select a metric', metrics, key="repo-metrics") 
-    with right:
-        with st.spinner('Loading word cloud...'):
-            wc = WordCloud(background_color="white", max_words=1000, width=800, height=500)
-            df = get_repos_df(metric)
-            wc.generate_from_frequencies({item[1]: item[2] for item in df.values})
-            st.image(wc.to_array())
+    st.subheader("Most active repositories")
+    with st.spinner('Loading word cloud...'):
+        wc = WordCloud(background_color="white", max_words=1000, width=800, height=500)
+        df = get_repos_df(metric, time_range[timestamp])
+        wc.generate_from_frequencies({item[1]: item[2] for item in df.values})
+        st.image(wc.to_array())
 
 
